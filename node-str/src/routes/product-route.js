ec747462633eccd6103a61df011dc0e68fc6de09
Samuel de Abreu Moraes <samu.steel@hotmail.com>
2019-11-18 11:29:52 -0300

    adicionando controller, repositorio e validação

--- node-str/src/routes/product-route.js
+++ node-str/src/routes/product-route.js
@@ -2,9 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
-const productController = require('../contollers/product-controller');
+const productController = require('../controllers/product-controller');
 
 router.get('/', productController.get);
+router.get('/:slug',productController.getBySlug);
+router.get('/admin/:id',productController.getById);
+router.get('/tags/:tag',productController.getByTag);
 router.post('/', productController.post);
 router.put('/:id',productController.put);
 router.delete('/:id',productController.delete);
